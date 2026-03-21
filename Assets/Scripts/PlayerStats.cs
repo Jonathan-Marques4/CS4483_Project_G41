02857d07f79602a18d639d1bff5f3b2961029f63
Derek <epnightmare02@gmail.com>
2026-03-21 03:04:07 -0400

    Added Player and playerstats

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,18 +1,71 @@
 using UnityEngine;
 
-public class PlayerStats : MonoBehaviour
+public class PlayerStats: MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float maxHp;
     public float currentHp;
+    public float armor;
+
+    public float moveSpeed;
+    public float sprintSpeed;
+    public float maxSprintEnergy;
+    public float currentSprintEnergy;
+
+    private bool dead;
     void Start()
     {
         currentHp = maxHp;
+        dead = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void takeDmg(float dmg)
     {
-        
+        if (dmg - armor < 1)
+        {
+            dmg = 1;
+        }
+        else
+        {
+            dmg -= armor;
+        }
+
+        currentHp -= dmg;
+
+        if (currentHp <= 0)
+        {
+            dead = true;
+            Debug.Log("Dead");
+        }
+    }
+
+    public float getCurrentHp()
+    {
+        return currentHp;
+    }
+
+    public void setCurrentHp(float hp)
+    {
+        currentHp = hp;
+    }
+
+    public float getMaxHp()
+    {
+        return maxHp;
+    }
+
+    public void setMaxHp(float hp)
+    {
+        maxHp = hp;
+    }
+
+    public float getArmor()
+    {
+        return armor;
+    }
+
+    public void setArmor(float armor)
+    {
+        this.armor = armor;
     }
 }
